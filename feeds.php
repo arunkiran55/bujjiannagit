7fe97b7426e2e4d77626f007e748b7dcb1fed636
arunkiran55 <ankr661@gmail.com>
2022-09-04 21:17:51 +0530

    commit first

---
+++ feeds.php
@@ -0,0 +1,12 @@
+<?php include("common/header.php"); ?>
+<div class="row feeds_section">
+    <div class="col-md-3 doctor_section"></div>
+    <div class="col-md-6">
+        <div class="card"></div>
+    </div>
+    <div class="col-md-3 doctor_section"></div>
+</div>
+<?php include("common/footer.php"); ?>
+<script>
+$(".footer_section .row").css('display', 'none');
+</script>
